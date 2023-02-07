ee7ad6c51bc2fe4ced91513e32de5a3f488eace7
eppsicle <70301308+eppsicle@users.noreply.github.com>
2023-02-07 15:10:12 -0700

    added comments to code

--- alt-pong-sketch.js
+++ alt-pong-sketch.js
@@ -1,25 +1,41 @@
 let serial;
 let latestData = "waiting for data";
 
+//initialize the sensor value
 let sensor1 = 0;
 
+//initialize sound effect variable
+let soundEffect;
+
+
+//initialize the timer to track the speed of the ball
 let speedTimer = 0;
 
+//initialize the size of the screen
 let screenWidth = 600;
 
+//initialize the scores of players 1 and 2
 let score1 = 0;
 let score2 = 0;
 
+//initialize the end game condition
 let gameOver = false;
 
+//initialize the x and y position and speed of the ball
 var xBall = 300;
 var yBall = 100;
-var xSpeed = 4;
-var ySpeed = -2;
+var xSpeed = 0.5;
+var ySpeed = -0.5;
 
 function setup() {
+  //make the screen for the game
   createCanvas(screenWidth, screenWidth / 2);
-
+  
+  //load bouncing sound effect
+  soundEffect = loadSound('pong-bounce.mp3');
+  
+  //create a group called dividers that draws a dotted line
+  //across the middle of the screen
   dividers = new Group();
   dividers.height = screenWidth / 160;
   dividers.width = screenWidth / 160;
@@ -32,27 +48,32 @@ function setup() {
     let divider = new dividers.Sprite();
     divider.y = dividers.length * 10;
   }
-
+  
+  
+  //create the first paddle sprite and set its color, position, etc.
   paddle1 = new Sprite();
   paddle1.height = screenWidth / 7;
   paddle1.width = screenWidth / 49;
   paddle1.collider = "static";
   paddle1.color = "white";
   paddle1.x = screenWidth / 50;
-
+  
+  //create the second paddle sprite and set its color, position, etc.
   paddle2 = new Sprite();
   paddle2.height = screenWidth / 7;
   paddle2.width = screenWidth / 49;
   paddle2.collider = "static";
   paddle2.color = "white";
   paddle2.x = screenWidth - screenWidth / 50;
-
+  
+  //create the ball sprite and set its size, color, etc. 
   ball = new Sprite();
   ball.height = screenWidth / 49;
   ball.width = screenWidth / 49;
   ball.collider = "dynamic";
   ball.color = "white";
-
+  
+  //create top wall sprite and set its size, location, etc.
   topWall = new Sprite();
   topWall.height = 20;
   topWall.width = screenWidth;
@@ -61,6 +82,7 @@ function setup() {
   topWall.y = -10;
   topWall.color = "black";
 
+  //create bottom wall sprite and set its size, location, etc.
   bottomWall = new Sprite();
   bottomWall.height = 20;
   bottomWall.width = screenWidth;
@@ -131,32 +153,47 @@ function gotData() {
 }
 
 function bounce() {
-  // if (xBall < 10 ||
-  //   xBall > 400 - 10) {
-  //   xSpeed *= -1;
-  // }
+  //if the ball gets close to the top or bottom walls, reverse the y-velocity of the ball
   if (yBall < 10 || yBall > screenWidth / 2 - 10) {
     ySpeed *= -1;
+    //play sound effect
+    soundEffect.play()
   }
 }
 
 function move() {
+  //add the x and y speed values to the respective x and y positions of the ball
   xBall += xSpeed;
   yBall += ySpeed;
 }
 
 function bouncePaddle() {
-  if (abs(yBall - paddle2.y) <= paddle2.height && abs(paddle2.x - xBall) < 5) {
+  
+  //if the ball is along the length of a paddle 2 and within 5 pixels of paddle 2...
+  if (abs(yBall - paddle2.y) <= paddle2.height/2 && abs(paddle2.x - xBall) < 5) {
+    
+    //play sound effect
+    soundEffect.play()
+    //reverse the x-velocity of the ball
     xSpeed *= -1;
+    //spit out a random number between 1 and 0
     coinFlip = Math.floor(Math.random() * 2);
+    //if that number is 1, also reverse the y-velocity of the ball
     if (coinFlip == 1){
       ySpeed *= -1;
     }
   }
 
-  if (abs(yBall - paddle1.y) <= paddle1.height && abs(paddle1.x - xBall) < 5) {
+  //if the ball is along the length of a paddle 1 and within 5 pixels of paddle 1...
+  if (abs(yBall - paddle1.y) <= paddle1.height/2 && abs(paddle1.x - xBall) < 5) {
+    
+    //play sound effect
+    soundEffect.play()
+    //reverse the x-velocity of the ball
     xSpeed *= -1;
+    //spit out a random number between 1 and 0
     coinFlip = Math.floor(Math.random() * 2);
+    //if that number is 1, also reverse the y-velocity of the ball
     if (coinFlip == 1){
       ySpeed *= -1;
     }
@@ -164,25 +201,27 @@ function bouncePaddle() {
 }
 
 function movePaddles() {
-//   if (keyboard.pressing("w") && paddle1.y >= screenWidth / 13) {
-//     paddle1.y -= 5;
-//     paddle2.y -= 5;
-//   }
+  if (keyboard.pressing("w") && paddle1.y >= screenWidth / 13) {
+    paddle1.y -= 5;
+    paddle2.y -= 5;
+  }
 
-//   if (
-//     keyboard.pressing("s") &&
-//     paddle1.y <= screenWidth / 2 - screenWidth / 13
-//   ) {
-//     paddle1.y += 5;
-//     paddle2.y += 5;
-//   }
+  if (
+    keyboard.pressing("s") &&
+    paddle1.y <= screenWidth / 2 - screenWidth / 13
+  ) {
+    paddle1.y += 5;
+    paddle2.y += 5;
+  }
   
-  mappedSensor = map(sensor1, 0, 1023, screenWidth/13, screenWidth/2 - screenWidth/13);
-  paddle1.y = mappedSensor;
-  paddle2.y = mappedSensor;
-}
+  //map the value of the potentiometer to a vertical position that the paddles will be set to
+  // mappedSensor = map(sensor1, 0, 517, screenWidth/13, screenWidth/2 - screenWidth/13);
+  // paddle1.y = mappedSensor;
+  // paddle2.y = mappedSensor;
+ }
 
 function setBall() {
+  //tell the ball what its position and speed is
   ball.x = xBall;
   ball.y = yBall;
   ball.vel.x = xSpeed;
@@ -190,45 +229,67 @@ function setBall() {
 }
 
 function score() {
+  //if the ball goes past the right paddle...
   if (xBall > screenWidth + 50) {
+    //reset the ball's location and speed
     xBall = 300;
     yBall = 100;
-    xSpeed = (1, 4);
-    ySpeed = (-8, -2);
+    xSpeed = 0.5;
+    ySpeed = -0.5;
+    
+    //add one point to player 1's score
     score1 += 1;
   }
+  
+  //if the ball goes past the left paddle...
   if (xBall < -50) {
+    //reset the ball's location and speed
     xBall = 300;
     yBall = 100;
-    xSpeed = (-1, -4);
-    ySpeed = (-8, -2);
+    xSpeed = 0.5
+    ySpeed = 0.5;
+    
+    //add one point to player 2's score
     score2 += 1;
   }
 }
 
 function increaseSpeed() {
+  //add one to the speed timer every time the draw function runs, which is 60 times per second
   speedTimer += 1;
+  
+  //if a period of 5 seconds has passed...
   if ((speedTimer / 60) % 5 == 0) {
-    xSpeed *= 1.1;
+    //increase the x and y speed of the ball
+    xSpeed *= 1.15;
     ySpeed *= 1.1;
   }
 }
 
+
 function draw() {
+  //reset the screen
   clear();
   background(0);
   noStroke();
-
+  
   //set up the text style
   textFont("Helvetica", screenWidth / 15);
   textStyle("bold");
   textAlign("center");
   fill("white");
 
+  if (score1 == 10 || score2 == 10){
+    if (keyboard.presses(' ')){
+      
+    }
+  
+  }
   //write the endGame text on the screen
   text(score1, screenWidth / 3, screenWidth / 14);
   text(score2, (2 * screenWidth) / 3, screenWidth / 14);
-
+  
+  //call all of the functions that run the game
   movePaddles();
   setBall();
   bounce();
